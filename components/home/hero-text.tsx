7d4fe0a92b127b16b8bb1d0e10363ac3568431b6
Shadman Shakib <sm.shakib007@gmail.com>
2024-10-23 21:03:51 +0600

    ui updated

---
+++ components/home/hero-text.tsx
@@ -0,0 +1,14 @@
+import TextRotator from "./text-rolator";
+
+const HeroTexts = () => {
+  return (
+    <>
+      <h3 className="font-poppins text-2xl max-sm:text-xl">My Name is</h3>
+      <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl ">
+        Dr Sadman <br /> Shakib .
+      </h1>
+      <TextRotator />
+    </>
+  );
+};
+export default HeroTexts;
