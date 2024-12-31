c20d699deed82c0d0177843df25b9fa05fc0cfaa
Shadman Shakib <sm.shakib007@gmail.com>
2024-12-31 18:38:50 +0600

    pages updated

--- app/(my-app)/projects/services.ts
+++ app/(my-app)/projects/services.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getProjects = async () => {
   const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/projects?limit=4?sort=published_at:asc`
+    `${process.env.NEXT_PUBLIC_API_URL}/projects?limit=4?sort=published_at`
   );
   return response.data;
 };
