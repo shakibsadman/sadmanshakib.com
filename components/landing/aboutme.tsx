25d49d8e698ba12f1c49cac9da0ef40d7bd5fc75
Shadman Shakib <sm.shakib007@gmail.com>
2025-01-04 23:40:45 +0600

    about me added

--- components/landing/aboutme.tsx
+++ components/landing/aboutme.tsx
@@ -17,19 +17,28 @@ export default function AboutMe() {
             height={500}
           />
           <div className="text-white w-1/2">
-            <p className="text-[16px] leading-relaxed">
-              Hi, I&apos;m Sadman Shakib, a passionate Full Stack Developer dedicated to turning
-              ideas into functional, scalable web applications. With a strong foundation in
-              TypeScript, Next.js, React, and Tailwind CSS, I specialize in building modern,
-              responsive, and user-friendly web solutions. My expertise lies in developing robust
-              front-end interfaces and seamless back-end systems, ensuring a smooth user experience
-              from start to finish. I thrive on helping entrepreneurs and businesses bring their to
-              finish. I thrive on helping entrepreneurs and businesses bring their SaaS ideas to
-              life, offering tailored MVPs that are both efficient and scalable. Whether you&apos;re
-              launching your first product or need a reliable developer to elevate your existing
-              project, I&apos;m here to make it happen. Let&apos;s collaborate to build something
-              amazing!
-            </p>
+            <div className="text-[16px] leading-relaxed">
+              <p>
+                Hi, I&apos;m Sadman Shakib, a passionate Full Stack Developer dedicated to turning
+                ideas into functional, scalable web applications. With a strong foundation in
+                TypeScript, Next.js, React, and Tailwind CSS, I specialize in building modern,
+                responsive, and user-friendly web solutions.
+              </p>
+              <br />
+              <p>
+                My expertise lies in developing robust front-end interfaces and seamless back-end
+                systems, ensuring a smooth user experience from start to finish. I thrive on helping
+                entrepreneurs and businesses bring their to finish. I thrive on helping
+                entrepreneurs and businesses bring their SaaS ideas to life, offering tailored MVPs
+                that are both efficient and scalable.
+              </p>
+              <br />
+              <p>
+                Whether you&apos;re launching your first product or need a reliable developer to
+                elevate your existing project, I&apos;m here to make it happen. Let&apos;s
+                collaborate to build something amazing!
+              </p>
+            </div>
           </div>
         </div>
       </div>
